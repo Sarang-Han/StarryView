43ac35d0eca89a847b7a47fea72d42630cbde7a8
Sarang <hangpfm0518@ewhain.net>
2024-12-15 14:23:44 +0900

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: 'https://starry-two.vercel.app/' }} // 배포한 웹 페이지 URL
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
